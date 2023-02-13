c8a8bec9cfbf028f68bf90889b6019373b09d300
David Granger <dpgranger8@gmail.com>
2023-02-13 11:04:25 -0700

    completed new struct implementation/black diamond assignment fully

---
+++ DetailViewer.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewer.swift
+//  IntroduceFamily
+//
+//  Created by David Granger on 2/13/23.
+//
+
+import UIKit
+
+class DetailViewer: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
